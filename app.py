46a354385b79e22c41cab998946492f1b99826f6
Lars Schöning <lars@lyschoening.de>
2016-05-04 10:45:00 +0200

    Add worker

--- app.py
+++ app.py
@@ -3,13 +3,23 @@ import asyncio
 from aiohttp import web
 from aiozmq import rpc
 
-async def hello(request):
-    client = await rpc.connect_rpc(connect='tcp://worker:5555')
+client = None
 
+async def hello(request):
     sentence = await client.call.make_a_sentence('Hello', 'world')
     return web.Response(text=sentence)
 
-
 app = web.Application()
 app.router.add_route('GET', '/', hello)
-web.run_app(app)
+
+
+async def server(loop):
+    global client
+    client = await rpc.connect_rpc(connect='tcp://worker:5555')
+    await loop.create_server(app.make_handler(), '0.0.0.0', 8080)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(server(loop))
+    loop.run_forever()
